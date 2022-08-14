6a6b2775016bae10bb44dece37fd3e7d116d691a
unknown <ngoanhthu123123@gmail.com>
2022-08-14 20:58:49 +0700

    NFt-component

--- imgart/src/App.js
+++ imgart/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
-
+import {NFT} from './components/NFT/NFT';
 
 function App() {
   return (
     <div className="App">
+      <NFT src={"image.png"} name = {"Crazy Apes"} avt={"avt.png"} author ={"hx45...250e"} price = {"3.90"}></NFT>
     </div>
   );
 }
