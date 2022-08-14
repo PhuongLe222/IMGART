4049138fbb06330225e363f7e277cdeaef049521
unknown <ngoanhthu123123@gmail.com>
2022-08-14 21:36:11 +0700

    add title

--- imgart/src/App.js
+++ imgart/src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import {NFT} from './components/NFT/NFT';
-
+import CreateNFT from './container/CreateNFT/CreateNFT';
 function App() {
   return (
     <div className="App">
       <NFT src={"image.png"} name = {"Crazy Apes"} avt={"avt.png"} author ={"hx45...250e"} price = {"3.90"}></NFT>
+      <CreateNFT></CreateNFT>
     </div>
   );
 }
---
+++ imgart/src/container/CreateNFT/CreateNFT.jsx
@@ -0,0 +1,7 @@
+import "./CreateNFT.css";
+
+const CreateNFT = () => {
+  return <p className="title">Create & Sell Your NFTs</p>;
+};
+
+export default CreateNFT;
