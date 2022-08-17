d29ac7b931753642acec06c51db78e9bc868c94e
Trân Trần Gia <19522377@ms.uit.edu.vn>
2022-08-17 10:47:42 +0700

    edit relatively responsive for header-component

--- imgart/src/App.js
+++ imgart/src/App.js
@@ -1,16 +1,10 @@
 import './App.css';
-import { Header } from './components/Header';
-import { Intro } from './components/Intro';
-import { Footer } from './components/Footer/Footer';
 
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Intro/>
-      <Footer/>
-
+     
     </div>
   );
 }
