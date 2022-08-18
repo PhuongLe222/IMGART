96f2dce88294211f52fbf6d571001128b8e96d11
PhuongLe222 <kkimphuong22@gmail.com>
2022-08-18 15:37:52 +0700

    merge layout-component

--- imgart/src/App.js
+++ imgart/src/App.js
@@ -1,16 +1,8 @@
 import './App.css';
-import { PrimaryLayout } from './components/Layout';
+
 function App() {
   return (
 
-      <PrimaryLayout>
-        <h1>hihihih</h1>
-        <h1>fdagfs</h1>
-        <h1>fdagfs</h1>
-        <h1>fdagfs</h1>
-        
-      </PrimaryLayout>
-  
   );
 }
 
