b2a0b8e24f7511d98cad047d9ddaa60425bf1d46
unknown <ngoanhthu123123@gmail.com>
2022-08-21 00:04:14 +0700

    add primary layout to container

--- imgart/src/container/CreateNFT/CreateNFT.jsx
+++ imgart/src/container/CreateNFT/CreateNFT.jsx
@@ -1,7 +1,7 @@
 import "./CreateNFT.css";
 import { Button } from "../../components/Button/Button";
 import { TopCreator } from "../../components/TopCreators/TopCreators";
-
+import { PrimaryLayout } from "../../components/Layout";
 const CreateNFT = () => {
   let NFTitem = [
     {
@@ -31,52 +31,59 @@ const CreateNFT = () => {
     },
   ];
   return (
-    <div>
-      <div className="left">
-        <p className="title">Create & Sell Your NFTs</p>
-        <div className="upload">
-          <img className="picture" src="upload.png" alt="UploadPicture" />
-          <p className="textUpload">Upload</p>
-          <div className="framedes">
-            <p className="desUpload">
-              {" "}
-              Upload your work, customize your NFTs with properties, stats, and
-              unlockable content.
-            </p>
+    <PrimaryLayout>
+      <div className="container">
+        <div className="left">
+          <p className="title">Create & Sell Your NFTs</p>
+          <div className="upload">
+            <img className="picture" src="upload.png" alt="UploadPicture" />
+            <p className="textUpload">Upload</p>
+            <div className="framedes">
+              <p className="desUpload">
+                {" "}
+                Upload your work, customize your NFTs with properties, stats,
+                and unlockable content.
+              </p>
+            </div>
+          </div>
+          <div className="btn-connect">
+            <Button
+              Height={"61"}
+              Width={"260"}
+              FSize={"25px"}
+              FontFamily={"Sniglet"}
+              FWeight={400}
+            >
+              Connect
+            </Button>
           </div>
         </div>
-        <div className="btn-connect">
-          <Button
-            Height={"61"}
-            Width={"260"}
-            FSize={"25px"}
-            FontFamily={"Sniglet"}
-            FWeight={400}
-          >
-            Connect
-          </Button>
+        <div className="right">
+          <p className="title-TopCreators">Top Creators of the week</p>
+          <ul className="list-TopCreators">
+            {NFTitem.map((item) => (
+              <li>
+                <TopCreator
+                  src={item.src}
+                  author={item.author}
+                  price={item.price}
+                ></TopCreator>
+              </li>
+            ))}
+          </ul>
+          <div className="btn-watchMore">
+            <Button
+              Height={61}
+              Width={260}
+              FSize={"25px"}
+              FontFamily={"Sniglet"}
+            >
+              Watch More
+            </Button>
+          </div>
         </div>
       </div>
-      <div className="right">
-        <p className="title-TopCreators">Top Creators of the week</p>
-        <ul className="list-TopCreators">
-          {NFTitem.map((item) => (
-            <li>
-              <TopCreator
-                src={item.src}
-                author={item.author}
-                price={item.price}
-              ></TopCreator>
-            </li>
-          ))}
-        </ul>
-        <div className="btn-watchMore">
-          <Button Height={61} Width={260} FSize={"25px"} FontFamily={"Sniglet"}>
-            Watch More
-          </Button>
-        </div>
-      </div>
-    </div>
+    </PrimaryLayout>
   );
 };
 
