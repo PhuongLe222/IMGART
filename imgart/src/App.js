49f6ea4b9aa1b2b04e93cc12b0574906d0dae120
Trang Lê Thị Thu <19522382@ms.uit.edu.vn>
2022-08-21 20:56:59 +0700

    add route in app.js and index.js

--- imgart/src/App.js
+++ imgart/src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
-
+import { Routes, Route } from 'react-router-dom'
+import Aboutus from './containers';
 function App() {
   return (
+    <Routes>
+      <Route path='/about-us' element={<Aboutus/>}/>
+    </Routes>
 
   );
 }
